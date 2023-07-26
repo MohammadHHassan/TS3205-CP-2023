713d509b976311079d4607e159106800adc9db5f
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2023-07-26 15:12:00 +0800

    Add files via upload

---
+++ day1/Exercise1.cpp
@@ -0,0 +1,21 @@
+/* 	Exercise 1
+	
+	Create a program which prints out:
+	
+	Baa baa black sheep,
+	Have you any wool?
+	Yes sir, Yes sir,
+	Three bags full.
+*/
+
+#include <stdio.h>			// Library for printf
+
+int main(void)				// Main function
+{
+	printf("Baa baa black sheep,\n");		// Prints out Baa baa black sheep,
+	printf("Have you any wool?\n");			// Prints out Have you any wool? on a new line
+	printf("Yes sir, Yes sir,\n");			// Prints out Yes sir, Yes sir, on a new line
+	printf("Three bags full.");				// Prints out Three bags full. on the last line
+	
+	return 0;
+}
---
+++ day1/LearningNewLines.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>			// Header file for input and output
+
+int main(void)				// This is the Main function
+{
+	printf("Hello World!\nGoodbye World :(");		// Print out the text Hello World! and Goodbye World :(
+	
+	return 0;
+}
